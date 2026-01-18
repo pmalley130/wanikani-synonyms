658b7715c6d1495536907b6c9affd1ff17831fe2
pmalley130 <pmalley130@gmail.com>
2026-01-18 16:57:03 -0500

    add wanikani ID to index

--- wanikani-synonyms.py
+++ wanikani-synonyms.py
@@ -12,7 +12,7 @@ DICT_PATH = os.getenv("DICT_PATH")
 INDEX_PATH = os.getenv("INDEX_PATH")
 
 #get the vocab of specific levels
-def get_vocab(levels: str) -> list[str]:
+def get_vocab(levels: str) -> list[dict]:
     #build url for request
     API_KEY = os.getenv("WANIKANI_API_KEY") 
     base_url = "https://api.wanikani.com/v2/subjects"
@@ -29,7 +29,7 @@ def get_vocab(levels: str) -> list[str]:
         params["levels"] = levels
 
     url = base_url
-    characters = [] #empty array of vocab to be returned
+    vocab = [] #empty array of vocab to be returned
 
     #if it's over 1000 entries it paginates, so need a loop
     while url:
@@ -37,19 +37,23 @@ def get_vocab(levels: str) -> list[str]:
 
         #extract vocab from response
         for item in response.get("data",[]):
+            id = item.get("id")
             char = item.get("data",{}).get("characters")
-            if char:
-                characters.append(char)
+            if id is not None and char:
+                vocab.append({
+                    "id":id,
+                    "characters": char
+                })
 
         #follow pagination due to 1000 api limit
         url = response.get("pages", {}).get("next_url")
 
-    return characters
+    return vocab
 
 #generate a lookup index that just holds the jp word and english defs from jmdict
-def generate_index(vocab: list[str]):
-    #set of wanikani vocab
-    vocab_set = set(vocab)
+def generate_index(vocab: list[dict]):
+    #set of wanikani vocab terms
+    vocab_terms = {item["characters"] for item in vocab if item.get("characters")}
 
     #open dictionary file
     with open(DICT_PATH, encoding="utf-8") as f:
@@ -61,7 +65,7 @@ def generate_index(vocab: list[str]):
     #index of 日本語 -> 英語
     index = defaultdict(list)
 
-    #walk through each entry
+    #walk through each dictionary entry
     for entry in words:
         terms = []
         
@@ -70,12 +74,12 @@ def generate_index(vocab: list[str]):
         if kanji_list:
             for k in kanji_list:
                 text = k.get("text")
-                if text and text in vocab_set:
+                if text and text in vocab_terms:
                     terms.append(text)
         else: #entries that are just kana (like あっさり) don't have a "kanji" entry but we still want them
             for k in entry.get("kana",[]):
                 text = k.get("text")
-                if text and text in vocab_set:
+                if text and text in vocab_terms:
                     terms.append(text)
 
         if not terms:
@@ -113,12 +117,27 @@ def generate_index(vocab: list[str]):
             if d not in seen:
                 seen.add(d)
                 unique.append(d)
+                if len(unique) == 5: #some common things like "上げる" have roughly one septillion possible definitions, so I've set an arbitrary cap
+                    break
         index[term] = unique
-    
-    #save the index for later loading
-    with open(INDEX_PATH, "wb") as f:
-        pickle.dump(index, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-levels = input("Which levels? (enter as a comma separated string, no spaces) ")
-vocab = get_vocab(levels)
-generate_index(vocab)
+    #add definition to original vocab list
+    for item in vocab:
+        term = item.get("characters")
+        if term in index:
+            item["definitions"] = index[term]
+    
+    #save the vocab list for later loading
+    with open(INDEX_PATH, "wb") as f:
+        pickle.dump(vocab, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+if not os.path.isfile(INDEX_PATH):
+    levels = input("Which levels? (enter as a comma separated string, no spaces) ")
+    vocab = get_vocab(levels)
+    generate_index(vocab)
+
+with open(INDEX_PATH, "rb") as f:
+    index = pickle.load(f)
+
+print(json.dumps(index,indent=3,ensure_ascii=False))
+
